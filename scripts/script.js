afe017358397a512cb743edd87caea0215ad34ad
samssjubair <samssjubair@gmail.com>
2021-02-07 22:37:53 +0600

    Some js comment added

--- scripts/script.js
+++ scripts/script.js
@@ -1,37 +1,32 @@
-document.getElementById('search-btn').addEventListener('click',()=>{
-    const searchedFood= document.getElementById('search-input').value;
-    document.getElementById('search-input').value="";
-    document.getElementById('result-section').innerHTML="";
-    if(searchedFood.length==1){
+//search button click handler
+function searchClickHandler(){
+    document.getElementById('search-btn').addEventListener('click',()=>{
+        const searchedFood= document.getElementById('search-input').value;
+        document.getElementById('search-input').value="";
+        document.getElementById('result-section').innerHTML="";
+        if(searchedFood.length==1){
+            
+            fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${searchedFood}`)
+            .then(res=>res.json())
+            .then(data=>displayResult(data.meals))
+            .catch(err=>noResult())
+        }
+        else{
+            fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchedFood}`)
+            .then(res=>res.json())
+            .then(data=>displayResult(data.meals))
+            .catch(()=>noResult())
+    
+        }
         
-        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${searchedFood}`)
-        .then(res=>res.json())
-        .then(data=>displayResult(data.meals))
-        .catch(err=>noResult())
-    }
-    else{
-        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchedFood}`)
-        .then(res=>res.json())
-        .then(data=>displayResult(data.meals))
-        .catch(()=>noResult())
-
-    }
-    
-})
+    })
+}
 
 
-// function displayResult(data){
-//     const resultSection= document.getElementById('result-section');
-//     const foodDiv=document.createElement('div');
-//     foodDiv.className='food-div';
-//     const divContent=` <img class="thumb-img" src='${data.meals[0].strMealThumb}'>
-//     <h2 class="food-title">${data.meals[0].strMeal}</h2>
-//     `;
-//     foodDiv.innerHTML=divContent;
-//     resultSection.appendChild(foodDiv);
-    
-// }
+searchClickHandler();
 
+
+//if promise is unsuccessful and no data is found
 function noResult(){
     const resultSection= document.getElementById('result-section');
     const foodDiv=document.createElement('div');
@@ -41,9 +36,9 @@ function noResult(){
     resultSection.appendChild(foodDiv);
 }
 
-function displayResult(foods){
-    // console.log(foods.meals);
 
+//display all the matched results
+function displayResult(foods){
     foods.forEach(food => {
         const resultSection= document.getElementById('result-section');
         const foodDiv=document.createElement('div');
@@ -61,26 +56,26 @@ function displayResult(foods){
 }
 
 
+//If any food is clicked(calling DISPLAY DETAIL function in basis where it's clicked)
 function foodClickHandler(){
     const allFoods=document.getElementById('result-section');
     allFoods.addEventListener('click',(event)=>{
-        
         if(event.target.tagName=="IMG"||event.target.tagName=="H3"){
             const clickedFoodDiv= event.target.parentNode;
             const clickedFood= clickedFoodDiv.querySelector('h3').innerText;
-            
             displayDetail(clickedFood);
         }
         else if(event.target.tagName=="DIV"){
             const clickedFoodDiv= event.target;
             const clickedFood= clickedFoodDiv.querySelector('h3').innerText;
-            
             displayDetail(clickedFood);
         }
     })
     
 }
 
+
+//display all ingredients by fetching from API
 function displayDetail(clickedFood){
     document.getElementById('result-section').style.display='none';
     document.getElementById('search-div').style.display='none';
@@ -92,7 +87,6 @@ function displayDetail(clickedFood){
                     if(element.strMeal==clickedFood){
                         document.getElementById('detail-image').setAttribute('src',element.strMealThumb);
                         document.getElementById('detail-header').innerText=clickedFood;
-                        
                         for (let i = 1; i < 21; i++) {
                             const list=document.createElement('li');
                             const measure= element['strMeasure'+i];
@@ -103,9 +97,7 @@ function displayDetail(clickedFood){
                             }
                             const listContent= `${measure} ${ingredient}`;
                             list.innerText=listContent;
-                            document.getElementById('ingredients-ul').appendChild(list);
-                            
-                                                    
+                            document.getElementById('ingredients-ul').appendChild(list);                         
                         }
                         document.getElementById('ingredient-title').innerText="Ingredients";
                         
